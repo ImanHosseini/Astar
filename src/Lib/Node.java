00626ed309c9c503f482e2eecc6abe94e7254830
IMAN <salvationsentinel@yahoo.com>
2017-03-07 17:57:58 +0330

    First step!

---
+++ src/Lib/Node.java
@@ -0,0 +1,10 @@
+package Lib;
+
+/**
+ * Created by L i o n on 3/7/2017.
+ * Seyed Iman Hosseini Zavaraki
+ * Github @ https://github.com/ImanHosseini
+ * Wordpress @ https://imanhosseini.wordpress.com/
+ */
+public class Node {
+}
